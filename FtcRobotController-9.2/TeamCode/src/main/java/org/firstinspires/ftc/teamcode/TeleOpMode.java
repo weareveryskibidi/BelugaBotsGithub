6923474e3d13e25a22be989a205f7d898ffe47f6
AarushTripathi <106599323+AarushTripathi@users.noreply.github.com>
2024-08-12 14:39:05 -0400

    Update TeleOpMode.java

--- FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -9,13 +9,14 @@ public class TeleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("lf_drive");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("rf_drive");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("lb_drive");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("rb_drive");
 
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         //test these to see if bot goes forward when told to, if fails then swap from right to left, or vice versa
 
         waitForStart();
@@ -29,9 +30,16 @@ public class TeleOpMode extends LinearOpMode {
 
             double deno = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y+x+rx) / deno;
-            double backLeftPower = (y-x+rx) / deno;
+            double backLeftPower = (-y+x-rx) / deno;
             double frontRightPower = (y-x-rx) / deno;
-            double backRightPower = (y+x-rx) / deno;
+           double backRightPower = (y+x-rx) / deno;
+
+//            double frontLeftPower = (y) / deno;
+//            double backLeftPower = (x) / deno;
+//            double frontRightPower = (rx) / deno;
+//            double backRightPower = (0) / deno;
+
+
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
