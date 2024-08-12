4adc1484654ae79112c7dc85c8c652d245eaa997
AarushTripathi <106599323+AarushTripathi@users.noreply.github.com>
2024-08-12 14:40:10 -0400

    Create AutonBlueBelugaBots.java

---
+++ FtcRobotController-9.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonBlueBelugaBots.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name = "auto Blue")
+public class AutonBlueBelugaBots extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lfDrive = null;
+    private DcMotor rfDrive = null;
+    private DcMotor lbDrive = null;
+    private DcMotor rbDrive = null;
+
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        lfDrive  = hardwareMap.get(DcMotor.class, "lf_drive");
+        rfDrive = hardwareMap.get(DcMotor.class, "rf_drive");
+        lbDrive  = hardwareMap.get(DcMotor.class, "lb_drive");
+        rbDrive = hardwareMap.get(DcMotor.class, "rb_drive");
+
+        lfDrive.setDirection(DcMotor.Direction.REVERSE);
+        rfDrive.setDirection(DcMotor.Direction.FORWARD);
+        lbDrive.setDirection(DcMotor.Direction.FORWARD);
+        rbDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+
+            lfDrive.setPower(-0.6);
+            rfDrive.setPower(0.6);
+            lbDrive.setPower(0.6);
+            rbDrive.setPower(-0.6);
+            sleep(1100);
+            lfDrive.setPower(0.6);
+            rfDrive.setPower(-0.6);
+            lbDrive.setPower(-0.6);
+            rbDrive.setPower(0.6);
+            sleep(500);
+            lfDrive.setPower(0.6);
+            rfDrive.setPower(0.6);
+            lbDrive.setPower(0.6);
+            rbDrive.setPower(0.6);
+            sleep(3000);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
