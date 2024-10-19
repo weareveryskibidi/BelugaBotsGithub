17a4d890bb79a8ed544a584006864740d345a9ad
AarushTripathi <106599323+AarushTripathi@users.noreply.github.com>
2024-10-19 15:42:41 -0400

    BBColorSensorHuskyLens.java

---
+++ FtcRobotController-9.2/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BBColorSensorHuskyLens.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "Sensor: HuskyLens", group = "Sensor")
+@Disabled
+public class BBColorSensorHuskyLens extends LinearOpMode {
+
+    private final int READ_PERIOD = 1;
+
+    private HuskyLens huskyLens;
+
+    @Override
+    public void runOpMode()
+    {
+        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
+
+        rateLimit.expire();
+
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        } else {
+            telemetry.addData(">>", "Press start to continue");
+        }
+
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+
+        telemetry.update();
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (!rateLimit.hasExpired()) {
+                continue;
+            }
+            rateLimit.reset();
+
+            HuskyLens.Block[] blocks = huskyLens.blocks();
+            telemetry.addData("Block count", blocks.length);
+            for (int i = 0; i < blocks.length; i++) {
+                telemetry.addData("Block", blocks[i].toString());
+            }
+
+            telemetry.update();
+        }
+    }
+}
